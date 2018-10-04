5d921a0bd47a2889dfa7822323eb0f25f1bbe3e2
alexthescott <43523995+alexthescott@users.noreply.github.com>
2018-10-03 20:52:57 -0700

    Frontend detail and formatting. Global array for Uri

--- Spotipy.py
+++ Spotipy.py
@@ -4,6 +4,7 @@ Alexander Scott
 October 2018
 Written to find songs in a playlist that are in a tempo range 
 """
+import sys
 import spotipy
 import spotipy.util as util
 import json
@@ -29,7 +30,7 @@ playlists = sp.user_playlists(username)
 # Functions --------------------------------------------------------------------------------------
 
 # Prints the tracks found on the 100 item page
-def show_tracks(results, a, low, high, total):
+def show_tracks(results, a, low, high, total, uriArray):
 	tempFiltered = []
 	twentyPercent = int(total/5)
 	twentyDone = False
@@ -47,7 +48,7 @@ def show_tracks(results, a, low, high, total):
 		tempUri = []
 		tempUri.append(track['uri'])
 		tempFeature = sp.audio_features(tempUri)
-		tempBPM = json.dumps(tempFeature[0]['tempo'])
+		tempBPM = float(json.dumps(tempFeature[0]['tempo']))
 		tempCurrent = a * 100 + i
 
 		if twentyDone == False and tempCurrent == twentyPercent:
@@ -56,65 +57,45 @@ def show_tracks(results, a, low, high, total):
 			print("------------")
 			twentyDone = True 
 
-		if fourtyDone == False and tempCurrent == fourtyPercent:
+		elif fourtyDone == False and tempCurrent == fourtyPercent:
 			print("")
 			print("40% Complete")
 			print("------------")
 			fourtyDone = True 
 
-		if sixtyDone == False and tempCurrent == sixtyPercent:
+		elif sixtyDone == False and tempCurrent == sixtyPercent:
 			print("")
 			print("60% Complete")
 			print("------------")
 			sixtyDone = True
 
-		if eightyDone == False and tempCurrent == eightyPercent:
+		elif eightyDone == False and tempCurrent == eightyPercent:
 			print("")
 			print("80% Complete")
 			print("------------")
 			eightyDone = True
 
-		if tempBPM <= high and tempBPM >= low:
-			print("BPM: " + str(tempBPM) + " - " + tempTitle + " by " + tempArtist)
-			tempFiltered.append(tempUri)
-	return tempFiltered 
+		if float(tempBPM) >= float(low) and float(tempBPM) <= float(high):
+			uriArray.append(json.dumps(tempUri))
+			nameQuotation = '"' + tempTitle + '"'
+			print("BPM: " + '%06.2f' % (tempBPM) + " - " + '{:<44}'.format(nameQuotation) + 'by ' + tempArtist[1:-1])
 
 
 # INPUT: username, Playlist_Id for analysis, TempoMin, TempoMax, # of songs in Playlist
 # OUTPUT: An array of Spotify Uri
 # Uses show_tracks() to "flip through the pages" of the object to displace entire list
-def get_playlist_tracks(username, playlist_id, low, high, total):
-	CollectedFilter = []
+def get_playlist_tracks(username, playlist_id, low, high, total, uriArray):
 	print(str(total) + " songs to filter")
+	print("")
 	a = 0
 	results = sp.user_playlist(username, playlist_id)
 	tracks = results['tracks']
-	CollectedFilter.append(show_tracks(tracks, a, low, high, total))
+	show_tracks(tracks, a, low, high, total, uriArray)
 	a = a + 1
 	while tracks['next']:
     		tracks = sp.next(tracks)
-    		CollectedFilter.append(show_tracks(tracks, a, low, high, total))
+    		show_tracks(tracks, a, low, high, total, uriArray)
     		a = a + 1
-	return CollectedFilter
-
-# Input for playlist 0 < i <= n, where n is # of User's playlists
-def askPlaylist():
-	while True:
-		# Ensure integer input
-		try:
-			choice = int(raw_input('Enter the number of the playlist you would like to analyze: '))
-			break
-		except:
-			print("Please enter number")
-	# Ensure choice is within range
-	if choice >= i: 
-		print("Your number is too high")
-		askPlaylist()
-	if choice <= 0:
-		print("Your number is too low")
-		askPlaylist()
-	else:
-		return choice
 
 # Main -------------------------------------------------------------------------------------------
 
@@ -125,50 +106,66 @@ print("Your Playlists")
 print("--------------")
 
 
-playlistId = [] 
-playlistTitles = []
-playlistLength = []
+pId = [] 
+pTitles = []
+pLength = []
+pUriArray = []
 
 # Assume 1st is a placeholder to avoid list off by one error
-playlistId.append('NULL')
-playlistTitles.append('NULL')
-playlistLength.append(0)
+pId.append('NULL')
+pTitles.append('NULL')
+pLength.append(0)
 
 # Print User's Playlists 
 for i, playlist in enumerate(playlists['items']):
     print(str(i + 1) + ") " + str(playlist['name']))
-    playlistTitles.append(playlist['name'])
-    playlistLength.append(playlist['tracks']['total'])
+    pTitles.append(playlist['name'])
+    pLength.append(playlist['tracks']['total'])
     print("	" + str(playlist['tracks']['total']) + " Tracks")
-    playlistId.append(playlist['id'])
+    pId.append(playlist['id'])
     i += 1
 
-# Record User's Choice
-playlistChoice = askPlaylist()
+# Record User's Playlist Choice
+while True:
+	pChoice = input('Enter Playlist Number: ')
+	if isinstance(pChoice, int) and float(pChoice) > 0 and float(pChoice) <= i:
+		break
+	else:
+		print('Incorrect input. Please enter a positive number within range')
+
+print("")
+print("Tempo Range")
+print("-----------")
 
 # Record User's tempoFloor
 while True:
-	tempoFloor = raw_input('Enter Tempo Floor: ')
-	if tempoFloor.isdigit() and tempoFloor >= 0:
+	tempoFloor = input('Enter Tempo Floor: ')
+	if isinstance(tempoFloor, int) and float(tempoFloor) >= 0:
 		break
 	else:
 		print('Incorrect floor input. Please enter a positive number')
 
 # Record User's tempoCeiling
 while True:
-	tempoCeiling = raw_input('Enter Tempo Ceiling: ')
-	if tempoCeiling.isdigit() and tempoCeiling >= 0:
+	tempoCeiling = input('Enter Tempo Ceiling: ')
+	if isinstance(tempoCeiling, int) and float(tempoCeiling) >= 0:
 		break
 	else:
 		print('Incorrect ceiling input. Please enter a positive number')
 
-# Record User's new Playlist Preference
-
 # Respond to Choice
 print("")
-print("Analyzing " + playlistTitles[playlistChoice] + " Playlist")
+print("Analyzing " + pTitles[pChoice] + " Playlist")
 print("")
 
 # Analyize and use Choice
-Choice = get_playlist_tracks(username, playlistId[playlistChoice], tempoFloor, tempoCeiling, playlistLength[playlistChoice])
-print(json.dumps(Choice[0], indent = 4))
+get_playlist_tracks(username, pId[pChoice], tempoFloor, tempoCeiling, pLength[pChoice], pUriArray)
+
+print("")
+for t in pUriArray:
+	print(t[2:-2])
+
+print("")
+percentInRange = len(pUriArray)/float(pLength[pChoice]) * 100
+print('%.2f'%percentInRange + "% of songs in " + pTitles[pChoice] + " are in the range " + str(tempoFloor) + "-" + str(tempoCeiling) + (" (BPM)"))
+print("")
